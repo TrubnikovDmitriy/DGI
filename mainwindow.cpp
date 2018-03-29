dfdef6f3ef9cf96c530c61da9dc158f5d8986af7
Dmitriy Trubnikov <trubnikovdv@mail.ru>
2018-03-29 11:55:12 +0300

    update

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,6 +1,7 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include <QDebug>
+#include <QString>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -14,11 +15,23 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+int comparator(const void* left, const void* right) {
+    QString left_str = *(QString*)left;
+    QString right_str = *(QString*)right;
+    return (left_str > right_str);
+}
+
 void MainWindow::on_pushButton_clicked()
 {
     QStringList list = ui->plainTextEdit->toPlainText().split('\n');
 
+
+
+    qsort(&list[0], list.size(), sizeof(list[0]), comparator);
+
+    ui->plainTextEdit->clear();
     for (int i = 0; i < list.size(); ++i) {
         qDebug() << list[i];
+        ui->plainTextEdit->appendPlainText(list[i]);
     }
 }
